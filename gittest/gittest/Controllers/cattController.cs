e3cb69b0c144b57a07e39b10caff2735d4251dde
okoaoko <jay87111385830@gmail.com>
2021-06-03 11:53:33 +0800

    新增檢視

---
+++ gittest/gittest/Controllers/cattController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gittest.Controllers
+{
+    public class cattController : Controller
+    {
+        // GET: catt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
